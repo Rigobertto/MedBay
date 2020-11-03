2491a327b80475054ef3492cbd537b039027339d
Bruno Vinícius <bruno.vinicius.72@gmail.com>
2020-11-03 20:44:35 -0300

    Implementação inicial da classe Prontuario.

---
+++ src/projeto/model/vo/Prontuario.java
@@ -0,0 +1,42 @@
+package projeto.model.vo;
+
+import java.util.Calendar;
+
+public class Prontuario {
+    private int id;
+    private Calendar data;
+
+    // private Medico medico;
+    // private String dados;
+    private String observacao;
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public boolean setId(int id) {
+        if(id < 0) return false;
+        else this.id = id;
+        return true;
+    }
+
+    public Calendar getData() {
+        return this.data;
+    }
+
+    public boolean setData(Calendar data) {
+        if(data == null) return false;
+        this.data = data;
+        return true;
+    }
+
+    public String getObservacao() {
+        return this.observacao;
+    }
+
+    public boolean setObservacao(String texto) {
+        if(texto == null) if(texto.isEmpty()) return false;
+        this.observacao = texto.toUpperCase();
+        return true;
+    }
+}
