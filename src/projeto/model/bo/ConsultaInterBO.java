5cbb49968221abc8a60163517ede1757597f1a04
Bruno Vinícius <bruno.vinicius.72@gmail.com>
2020-12-01 21:41:10 -0300

    Criação das interfaces InterBO de Consulta, Exame e Prontuario.

---
+++ src/projeto/model/bo/ConsultaInterBO.java
@@ -0,0 +1,16 @@
+package projeto.model.bo;
+
+public interface ConsultaInterBO {
+    static boolean cadastrar(ConsultaVO consulta); // Cadastra uma consulta
+    static boolean cadastrar(ConsultaVO consulta, ExameVO exame); // Vincula um exame a uma consulta
+
+    static boolean editar(ConsultaVO consulta, String observacao);
+    static boolean editar(ConsultaVO consulta, Calendar data_consulta);
+
+    static ConsultaVO buscar(int id); // id da consulta
+    static ConsultaVO [] buscar(String nome); // nome do paciente e ou do médico
+    static ConsultaVO [] buscar(CPF cpf); // cpf do paciente e ou do médico
+    static ConsultaVO [] buscar(Calendar data); // "marcados em" e "marcados para"
+    static ConsultaVO [] buscar(MedicoVO medico); // busca por id_medico
+    static ConsultaVO [] buscar(PacienteVO paciente) // busca por id_paciente
+}
