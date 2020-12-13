17eb5837966167ea85e711104ed87fce6cc872b0
Bruno Vinícius <bruno.vinicius.72@gmail.com>
2020-12-13 03:36:42 -0300

    Adição do protótipo dos métodos da classe util.Time() para conversão de String para Calendar e de Calendar para String.

---
+++ src/medbay/model/util/Time.java
@@ -0,0 +1,13 @@
+package medbay.model.util;
+
+import java.util.Calendar;
+
+class Time {
+    public static Calendar toCalendar(String data) {
+    	return Calendar.getInstance();
+    }
+
+    public static String toString(Calendar data) {
+        return new String();
+    }
+}
