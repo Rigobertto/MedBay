726e7885698b826583984b31c1996de5edf55590
Rigoberto Fernandes <rigo.rfp@hotmail.com>
2020-12-14 21:00:31 -0300

    gg

--- src/medbay/model/bo/GerenteBO.java
+++ src/medbay/model/bo/GerenteBO.java
@@ -12,19 +12,22 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class GerenteBO implements GerenteInterBO{
-	 Document document = new Document();
-	try {
-	PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\rigor\\OneDrive\\Documentos\\repositorios\\MedBay\\PDF_DevMedia.pdf");
-    document.open();
-
-    // adicionando um parágrafo no documento
-    document.add(new Paragraph("Gerando PDF - Java"));
+	
+	public void gerarPDF() {
+		try {
+			Document document = new Document();
+		PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\rigor\\OneDrive\\Documentos\\repositorios\\MedBay\\PDF_DevMedia.pdf");
+	    document.open();
+	
+	    // adicionando um parágrafo no documento
+	    document.add(new Paragraph("Gerando PDF - Java"));
+		}
+		catch(DocumentException de) {
+		    System.err.println(de.getMessage());
+		}
+		catch(IOException ioe) {
+		    System.err.println(ioe.getMessage());
+		}
+		document.close();
 	}
-	catch(DocumentException de) {
-	    System.err.println(de.getMessage());
-	}
-	catch(IOException ioe) {
-	    System.err.println(ioe.getMessage());
-	}
-	document.close();
 }
